e9240c48393121da256d4cb228db23938e69ebc3
joserc269510 <jose26jose26>
2014-10-09 08:23:50 -0600

    arreglos a agregar
    ya sirve

--- BancoBac/src/bancobac/ClasePruebaMatriz.java
+++ BancoBac/src/bancobac/ClasePruebaMatriz.java
@@ -5,37 +5,43 @@ public class ClasePruebaMatriz {
     public int maxsize;
     public String[][]arreglo;
     public final int MAX_SIZE = 10;
+   
     
     public ClasePruebaMatriz(){
         this.size = 0;
         this.maxsize = MAX_SIZE;
         this.arreglo = new String[10][5];
+   
 }
-    public void agregar(String cliente){
-        int c = 0;
+    public void agregar(String cliente, String correo, String tipo, String fecha, String hora){
+        
         
         if (this.size < this.maxsize) {
             
             if (this.size<=4){
-                this.arreglo[c][this.size] = cliente;
+                
+                this.arreglo[this.size][0] = cliente;
+                this.arreglo[this.size][1] = correo;
+                this.arreglo[this.size][2] = tipo;
+                this.arreglo[this.size][3] = fecha;
+                this.arreglo[this.size][4] = hora;
+                
                 this.size++;
             }
-            else{                
-                this.size = 0;
-                if (this.size < 5) {
-                    ++c;
-                    this.arreglo[c][this.size] = cliente;
-                    this.size++;
-            }
+        
         }
+        
     }
-    }
-    public String getElemento(int indice, int indice2) throws Exception{
+        
+   
+   
+    public String getElemento(int indice, int indice2) {
         
         
         return arreglo[indice][indice2];
         
-    } 
+    }
+    
     
     public void eliminarymostrar(){
         int i = 0;
@@ -54,21 +60,15 @@ public static void main(String[] args) throws Exception {
         
         ClasePruebaMatriz arreglo = new ClasePruebaMatriz();
         
-        arreglo.agregar("holaA");
-        arreglo.agregar("ADIOS");
-        arreglo.agregar("abc");
-        arreglo.agregar("sisis");
-        arreglo.agregar("nonono");
+        arreglo.agregar("nombre1", "correo1", "tipo1", "fecha1" , "hora1");
+ 
         
-        arreglo.agregar("0 de indice 1");
-        arreglo.agregar("1 de indice 1");
-        arreglo.agregar("2 de indice 1");
-        arreglo.agregar("3 de indice 1");
-        arreglo.agregar("4 de indice 1");
+        arreglo.agregar("nombre2" , "correo2" , "tipo2" , "fecha2" , "hora2");
+
         
-        arreglo.agregar("vaaamos");
+        arreglo.agregar("nombre3" , "correo3" , "tipo3" , "fecha3" ,"hora3");
        
-        String elem = arreglo.getElemento(1,4);
+        String elem = arreglo.getElemento(2,2);
         System.out.println(elem);
                                                
     }
