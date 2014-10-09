3a783bb8675737a36e0ad760defe2e14f36286bd
estebanmorales1594 <estebanmorales1594@hotmail.com>
2014-10-09 14:32:14 -0600

    Mejoras a Graficos

--- BancoBac/src/bancobac/PrincipalGraficos.java
+++ BancoBac/src/bancobac/PrincipalGraficos.java
@@ -8,17 +8,14 @@ public class PrincipalGraficos extends JApplet {
     JTabbedPane pestañas;
     PastelGraficos pastelgrafico;
     BarraGraficos barragrafico;
-    LineaGraficos lineagrafico;
  
     public void init(){
         pestañas=new JTabbedPane();
         pastelgrafico=new PastelGraficos();
         barragrafico=new BarraGraficos();
-        lineagrafico=new LineaGraficos();
 
         pestañas.addTab("Grafico de Pastel", pastelgrafico);
         pestañas.addTab("Grafico de Barra", barragrafico);
-        pestañas.addTab("Grafico de Linea", lineagrafico);
         add(pestañas);
     } 
 }
