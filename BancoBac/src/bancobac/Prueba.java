6fc8c990e5e7aa804acf089033f64cd5080263de
estebanmorales1594 <estebanmorales1594@hotmail.com>
2014-10-12 16:06:21 -0600

    Pruebas(Ignorar)

---
+++ BancoBac/src/bancobac/Prueba.java
@@ -0,0 +1,28 @@
+package bancobac;
+
+/**
+ *
+ * @author Esteban
+ */
+public class Prueba {
+    
+    public Prueba(){}
+    
+    public boolean Comparar(String valor1, String valor2){
+        int n1=valor1.length();
+        int n2=valor2.length();
+        if (n1<n2){
+            return true true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    public static void main(String[] args) { 
+        Prueba p=new Prueba();
+        boolean com=p.Comparar("Hola", "Adios");
+        System.out.println(com);
+    } 
+    
+}
---
+++ BancoBac/src/bancobac/Quicksort.java
@@ -0,0 +1,38 @@
+package bancobac;
+
+public class Quicksort{
+    int[] matrix;
+    public void Quicksort(int matrix[], int a, int b){
+        this.matrix = new int[matrix.length];
+        int buf;
+        int from = a;
+        int to = b;
+        int pivot = matrix[(from+to)/2];
+        do{
+            while(matrix[from] < pivot){
+                from++;
+            }
+            while(matrix[to] > pivot){
+                to--;
+            }
+            if(from <= to){
+                buf = matrix[from];
+                matrix[from] = matrix[to];
+                matrix[to] = buf;
+                from++; to--;
+            }
+        }
+        while(from <= to);
+            if(a < to){
+                Quicksort(matrix, a, to);
+            }
+            if(from < b){
+                Quicksort(matrix, from, b);
+            }
+        this.matrix = matrix;
+    }
+    
+    public static void main(String[] args){ 
+
+    }
+}
